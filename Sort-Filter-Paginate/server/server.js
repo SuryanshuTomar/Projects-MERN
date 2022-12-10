e40e142625dd123b8d1b472f193330c08b00f0e7
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-12-10 15:40:20 +0530

    adding routes to the server

--- Sort-Filter-Paginate/server/server.js
+++ Sort-Filter-Paginate/server/server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const dbConnect = require("./dbConnect");
+const movieRoutes = require("./routes/Movie");
 const cors = require("cors");
 const app = express();
-const dbConnect = require("./dbConnect");
 
 dbConnect();
 
 app.use(express.json());
 app.use(cors);
+
+app.use("/api", movieRoutes);
 
 const port = process.env.PORT || 8080;
 
