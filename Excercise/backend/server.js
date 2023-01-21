729419499daf51b2033c3f399082858d3a31215f
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-21 19:57:56 +0530

    starter file

--- Excercise/backend/server.js
+++ Excercise/backend/server.js
@@ -1,12 +1,26 @@
 // imports
+require("dotenv").config();
 const express = require("express");
 
 // Express app
 const app = express();
 
+// Global Middlewares
+app.use((req, res, next) => {
+	console.log(req.path, req.method);
+	next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+	res.json({
+		message: "Welcome to the Excercise App!!!",
+	});
+});
+
 // Listen to server
-const PORT = 3500;
-const HOST = "localhost";
-app.listen(PORT, HOST, () => {
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
+app.listen(PORT, HOSTNAME, () => {
 	console.log(`Listening to server on PORT: ${PORT}`);
 });
