41f2ffbb8a6d0d104205b272bcc244f258e840da
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-23 19:57:26 +0530

    added homepage route and navbar

--- Excercise/frontend/src/App.jsx
+++ Excercise/frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
 
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="App">
+			<Navbar />
+			<div className="pages">
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
