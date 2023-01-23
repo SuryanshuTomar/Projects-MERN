38519a944bb6a182c82df4405eef34e2f9da3b60
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-23 18:57:26 +0530

    controller for get all and single workout

--- Excercise/backend/controllers/workouts.controller.js
+++ Excercise/backend/controllers/workouts.controller.js
@@ -2,8 +2,35 @@
 const Workout = require("../models/workouts.model");
 
 // Get all workouts
+const getAllWorkout = async (req, res) => {
+	try {
+		const allWorkouts = await Workout.find({}).sort({ createdAt: -1 });
+
+		res.status(200).json({ status: "success", data: allWorkouts });
+	} catch (error) {
+		res.status(400).json({ status: "failed", error: error.message });
+	}
+};
 
 // Get a single workouts
+const getWorkout = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const workout = await Workout.findById(id);
+		if (!workout) {
+			return res.status(400).json({
+				status: "failed",
+				error: `Workout with id: ${id} does not exist !`,
+			});
+		}
+		res.status(200).json({ status: "success", data: workout });
+	} catch (error) {
+		res.status(400).json({
+			status: "failed",
+			error: `Workout with id: ${id} does not exist !`,
+		});
+	}
+};
 
 // Create a new workouts
 const createWorkout = async (req, res) => {
@@ -11,9 +38,9 @@ const createWorkout = async (req, res) => {
 
 	try {
 		const workout = await Workout.create({ title, reps, load });
-		res.status(201).json(workout);
+		res.status(201).json({ status: "success", data: workout });
 	} catch (error) {
-		res.status(400).json({ status: "failed", error: error.message });
+		res.status(404).json({ status: "failed", error: error.message });
 	}
 };
 
@@ -22,4 +49,4 @@ const createWorkout = async (req, res) => {
 // update a workout
 
 // exports
-module.exports = { createWorkout };
+module.exports = { getAllWorkout, getWorkout, createWorkout };
