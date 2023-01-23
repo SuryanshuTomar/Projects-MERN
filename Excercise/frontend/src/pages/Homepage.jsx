09e733eb5eb77a57f0a95a0171a3fcf9d444c9f1
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-23 23:05:55 +0530

    fetch data for all excercise

--- Excercise/frontend/src/pages/Homepage.jsx
+++ Excercise/frontend/src/pages/Homepage.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { excerciseFetch } from "../axios/ExcerciseFetch";
 
 function Homepage() {
+	const [workouts, setWorkouts] = useState(null);
+
+	useEffect(() => {
+		const fetchWorkouts = async () => {
+			const response = await excerciseFetch("/workouts");
+			const data = response.data;
+
+			if (data.status === "success") {
+				setWorkouts(data.data);
+			}
+		};
+		fetchWorkouts();
+	}, []);
+
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
+			<div className="workouts">
+				{workouts &&
+					workouts.map((workout) => (
+						<p key={workout._id}>{workout.title}</p>
+					))}
+			</div>
 		</div>
 	);
 }
