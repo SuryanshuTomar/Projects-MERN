6891aeed819cb8afdd891971201e77b6431ccd69
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-24 12:11:11 +0530

    wrapping app with the workout context provider

--- Excercise/frontend/src/main.jsx
+++ Excercise/frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { WorkoutContextProvider } from "./context/WorkoutContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<WorkoutContextProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</WorkoutContextProvider>
 	</React.StrictMode>
 );
