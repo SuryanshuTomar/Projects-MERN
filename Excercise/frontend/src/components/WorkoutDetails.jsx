417724320a261ba66e16a8d74392e09f50b91b26
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-24 12:29:52 +0530

    workouts context update for delete calls

--- Excercise/frontend/src/components/WorkoutDetails.jsx
+++ Excercise/frontend/src/components/WorkoutDetails.jsx
@@ -2,8 +2,20 @@
 // packages
 import React from "react";
 
+// axios
+import { excerciseFetch } from "../axios/ExcerciseFetch";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
+
 // component
 function WorkoutDetails({ workout }) {
+	const { dispatchWorkouts } = useWorkoutContext();
+
+	const clickHandler = async (event) => {
+		const response = await excerciseFetch.delete(`/workouts/${workout._id}`);
+
+		dispatchWorkouts({ type: "DELETE_WORKOUT", payload: response.data.data });
+	};
+
 	return (
 		<div className="workout-details">
 			<h4>{workout.title}</h4>
@@ -14,6 +26,7 @@ function WorkoutDetails({ workout }) {
 				<strong>Reps: </strong> {workout.reps}
 			</p>
 			<p>{workout.createdAt}</p>
+			<span onClick={clickHandler}>Delete</span>
 		</div>
 	);
 }
