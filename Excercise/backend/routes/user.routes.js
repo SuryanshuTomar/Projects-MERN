d7d6a8a5e4b65be8e350f8cab29c4da4f9346e85
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 11:11:22 +0530

    user router skeleton

---
+++ Excercise/backend/routes/user.routes.js
@@ -0,0 +1,14 @@
+// imports
+const express = require("express");
+
+// Create a router
+const router = express.Router();
+
+// Login Route
+router.post("/login", () => {});
+
+// Sign-Up Route
+router.post("/signup", () => {});
+
+// Exports
+module.exports = router;
