ca6d5fc7fc6182cdb2d92cb3cc11a59566992d8c
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 11:50:10 +0530

    added signup logic

--- Excercise/backend/controllers/user.controller.js
+++ Excercise/backend/controllers/user.controller.js
@@ -1,3 +1,6 @@
+// imports
+const User = require();
+
 // Login User Logic
 const loginUser = async (req, res) => {
 	res.status(200).json({ status: "success", message: "Logged In!!" });
@@ -5,7 +8,17 @@ const loginUser = async (req, res) => {
 
 // Signup User Logic
 const signupUser = async (req, res) => {
-	res.status(200).json({ status: "success", message: "Signed Up!!" });
+	const { email, password } = req.body;
+
+	// try-catch because we are using static signup method
+	// which might throw an error
+	try {
+		const user = await User.signup(email, password);
+
+		res.status(200).json({ status: "success", user });
+	} catch (error) {
+		res.status(400).json({ status: "failed", error: error.message });
+	}
 };
 
 module.exports = { loginUser, signupUser };
