0f5e386008919beecfa9eacc8222016b905a42a5
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 14:50:46 +0530

    refractored code for asyncwrapper & Custom error

--- Excercise/backend/controllers/workouts.controller.js
+++ Excercise/backend/controllers/workouts.controller.js
@@ -1,48 +1,36 @@
 // imports
 const mongoose = require("mongoose");
+
 const Workout = require("../models/workouts.model");
+const asyncWrapper = require("../middleware/async-wrapper");
+const { createCustomError } = require("../errors/custom-error");
 
 // Get all workouts
-const getAllWorkout = async (req, res) => {
-	try {
-		const allWorkouts = await Workout.find({}).sort({ createdAt: -1 });
-		res.status(200).json({ status: "success", data: allWorkouts });
-	} catch (error) {
-		res.status(400).json({ status: "failed", error: error.message });
-	}
-};
+const getAllWorkout = asyncWrapper(async (req, res, next) => {
+	const allWorkouts = await Workout.find({}).sort({ createdAt: -1 });
+	res.status(200).json({ status: "success", data: allWorkouts });
+});
 
 // Get a single workouts
-const getWorkout = async (req, res) => {
+const getWorkout = asyncWrapper(async (req, res, next) => {
 	const { id } = req.params;
 
 	// check if the id is a valid mongo id or not
 	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({
-			status: "failed",
-			error: `No Such Workout Exists! :  ${id}`,
-		});
+		return next(createCustomError(`No Such Workout Exists! :  ${id}`, 404));
 	}
 
-	try {
-		const workout = await Workout.findById(id);
-		if (!workout) {
-			return res.status(404).json({
-				status: "failed",
-				error: `Workout with id: ${id} does not exist !`,
-			});
-		}
-		res.status(200).json({ status: "success", data: workout });
-	} catch (error) {
-		res.status(400).json({
-			status: "failed",
-			error: error.message,
-		});
+	const workout = await Workout.findById(id);
+	if (!workout) {
+		return next(
+			createCustomError(`Workout with id: ${id} does not exist !`, 404)
+		);
 	}
-};
+	res.status(200).json({ status: "success", data: workout });
+});
 
 // Create a new workouts
-const createWorkout = async (req, res) => {
+const createWorkout = asyncWrapper(async (req, res, next) => {
 	const { title, reps, load } = req.body;
 
 	// Empty Fields response handling
@@ -51,84 +39,63 @@ const createWorkout = async (req, res) => {
 	if (!reps) emptyFields.push("reps");
 	if (!load) emptyFields.push("load");
 	if (emptyFields.length > 0) {
-		return res.status(400).json({
-			status: "failed",
-			error: "Please fill in all empty fields: " + emptyFields,
-			emptyFields,
-		});
+		return next(
+			createCustomError(
+				"Please fill in all empty fields: " + emptyFields,
+				404
+			)
+		);
 	}
 
-	try {
-		const workout = await Workout.create({ title, reps, load });
-		res.status(201).json({ status: "success", data: workout });
-	} catch (error) {
-		res.status(500).json({ status: "failed", error: error.message });
-	}
-};
+	const workout = await Workout.create({ title, reps, load });
+	res.status(201).json({ status: "success", data: workout });
+});
 
 // Delete a workout
-const deleteWorkout = async (req, res) => {
+const deleteWorkout = asyncWrapper(async (req, res, next) => {
 	const { id } = req.params;
 
 	// check if the id is a valid mongo id or not
 	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({
-			status: "failed",
-			error: `No Such Workout Exists! :  ${id}`,
-		});
+		return next(
+			createCustomError(`No Such Workout id Exists! :  ${id}`, 404)
+		);
 	}
 
-	try {
-		const deletedWorkout = await Workout.findByIdAndDelete({ _id: id });
-		if (!deletedWorkout) {
-			return res.status(404).json({
-				status: "failed",
-				error: `Workout with id: ${id} does not exist !`,
-			});
-		}
-		res.status(200).json({ status: "success", data: deletedWorkout });
-	} catch (error) {
-		res.status(400).json({
-			status: "failed",
-			error: error.message,
-		});
+	const deletedWorkout = await Workout.findByIdAndDelete({ _id: id });
+	if (!deletedWorkout) {
+		return next(
+			createCustomError(`Workout with id: ${id} does not exist !`, 404)
+		);
 	}
-};
+	res.status(200).json({ status: "success", data: deletedWorkout });
+});
 
 // update a workout
-const updateWorkout = async (req, res) => {
+const updateWorkout = asyncWrapper(async (req, res, next) => {
 	const { id } = req.params;
 
 	// check if the id is a valid mongo id or not
 	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({
-			status: "failed",
-			error: `No Such Workout Exists! :  ${id}`,
-		});
+		return next(
+			createCustomError(`No Such Workout id Exists! :  ${id}`, 404)
+		);
 	}
 
-	try {
-		const updateWorkout = await Workout.findOneAndUpdate(
-			{ _id: id },
-			{
-				...req.body,
-			},
-			{ new: true }
+	const updateWorkout = await Workout.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		},
+		{ new: true }
+	);
+	if (!updateWorkout) {
+		return next(
+			createCustomError(`Workout with id: ${id} does not exist !`, 404)
 		);
-		if (!updateWorkout) {
-			return res.status(404).json({
-				status: "failed",
-				error: `Workout with id: ${id} does not exist !`,
-			});
-		}
-		res.status(200).json({ status: "success", data: updateWorkout });
-	} catch (error) {
-		res.status(400).json({
-			status: "failed",
-			error: error.message,
-		});
 	}
-};
+	res.status(200).json({ status: "success", data: updateWorkout });
+});
 
 // exports
 module.exports = {
