c3157241e7e243fb37c891171cba80057d38db2e
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 11:07:26 +0530

    added cors origin

--- Excercise/backend/server.js
+++ Excercise/backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json()); // json parser
 app.use(
 	cors({
-		origin: "http://127.0.0.1:5173",
+		origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
 	})
 );
 
