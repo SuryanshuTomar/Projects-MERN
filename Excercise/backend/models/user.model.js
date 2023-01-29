167455e3dce124fe57aaddc1dfc0ace4b894860f
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 11:42:51 +0530

    static method signup for userschema

--- Excercise/backend/models/user.model.js
+++ Excercise/backend/models/user.model.js
@@ -1,5 +1,6 @@
 // imports
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Get the mongoose Schema Class
 const Schema = mongoose.Schema;
@@ -16,6 +17,26 @@ const userSchema = new Schema({
 		required: [true, "password is required!"],
 	},
 });
+
+// Static Signup Method
+userSchema.statics.signup = async (email, password) => {
+	// check if email already exists in db
+	const emailExist = await this.findOne({ email });
+	if (emailExist) throw new Error("email already exists!");
+
+	// Password hashing
+	// 1. Generate Salt
+	const salt = await bcrypt.genSalt(10); // genSalt(cost of salt)
+
+	// 2. Hash user password with salt
+	const hash = await bcrypt.hash(password, salt);
+
+	// Create the document with user email and hashed password
+	const user = await this.create({ email, password: hash });
+
+	// return the user
+	return user;
+};
 
 // Create a model from the userSchema
 const model = mongoose.Model("User", userSchema);
