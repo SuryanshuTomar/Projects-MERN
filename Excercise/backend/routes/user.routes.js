3361080ee53976035958bba9e476169b8657d5db
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-29 11:15:38 +0530

    added controller to the user router

--- Excercise/backend/routes/user.routes.js
+++ Excercise/backend/routes/user.routes.js
@@ -1,14 +1,15 @@
 // imports
 const express = require("express");
+const { loginUser, signupUser } = require("../controllers/user.controller");
 
 // Create a router
 const router = express.Router();
 
 // Login Route
-router.post("/login", () => {});
+router.post("/login", loginUser);
 
 // Sign-Up Route
-router.post("/signup", () => {});
+router.post("/signup", signupUser);
 
 // Exports
 module.exports = router;
