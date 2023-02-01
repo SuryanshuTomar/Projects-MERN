0ef1c4f7f760ca421bb2d28bc540edbbdd0ecc0a
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 12:37:51 +0530

    setting worksouts to null on logout

--- Excercise/frontend/src/hooks/useLogout.jsx
+++ Excercise/frontend/src/hooks/useLogout.jsx
@@ -1,14 +1,19 @@
 import { useAuthContext } from "./useAuthContext";
+import { useWorkoutContext } from "../hooks/useWorkoutsContext";
 
 export const useLogout = () => {
-	const { dispatch } = useAuthContext();
+	const { dispatch: authDispatch } = useAuthContext();
+	const { dispatchWorkouts: workoutDispatch } = useWorkoutContext();
 
 	const logout = () => {
 		// Remove user token from the local storage
 		localStorage.removeItem("user");
 
 		// Dispatch the LOGOUT action type to the AuthContext
-		dispatch({ type: "LOGOUT" });
+		authDispatch({ type: "LOGOUT" });
+
+		// Dispatch the SET_WORKOUTS action type and set the workouts to NULL
+		workoutDispatch({ type: "SET_WORKOUTS", payload: null });
 	};
 
 	return { logout };
