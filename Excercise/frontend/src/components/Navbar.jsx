40f479d6c74e228d6cc171951c43dcb2c9d55202
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 10:06:02 +0530

    added conditional rendering for login and logout

--- Excercise/frontend/src/components/Navbar.jsx
+++ Excercise/frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
 
 function Navbar() {
 	const { logout } = useLogout();
+	const { user } = useAuthContext();
 
 	return (
 		<header>
@@ -12,13 +14,18 @@ function Navbar() {
 					<h1>Excercise</h1>
 				</NavLink>
 				<nav>
-					<div>
-						<button onClick={logout}>Logout</button>
-					</div>
-					<div>
-						<NavLink to="/login">Login</NavLink>
-						<NavLink to="/signup">Signup</NavLink>
-					</div>
+					{user && (
+						<div>
+							<span>{user.email}</span>
+							<button onClick={logout}>Logout</button>
+						</div>
+					)}
+					{!user && (
+						<div>
+							<NavLink to="/login">Login</NavLink>
+							<NavLink to="/signup">Signup</NavLink>
+						</div>
+					)}
 				</nav>
 			</div>
 		</header>
