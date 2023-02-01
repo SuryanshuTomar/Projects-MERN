5983f3e7c8c476ff49218bbae48fd3e2bfc162ba
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 12:37:58 +0530

    Excercise app complete

--- Excercise/frontend/src/App.jsx
+++ Excercise/frontend/src/App.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+	const { user } = useAuthContext();
+
 	return (
 		<div className="App">
 			<Navbar />
 			<div className="pages">
 				<Routes>
-					<Route path="/" element={<Homepage />} />
-					<Route path="login" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
+					<Route
+						path="/"
+						element={user ? <Homepage /> : <Navigate to="/login" />}
+					/>
+					<Route
+						path="login"
+						element={!user ? <Login /> : <Navigate to="/" />}
+					/>
+					<Route
+						path="signup"
+						element={!user ? <Signup /> : <Navigate to="/" />}
+					/>
 				</Routes>
 			</div>
 		</div>
