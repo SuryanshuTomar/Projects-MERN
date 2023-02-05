475d7750d23eea32f4d1d1d009d80e8ede65e520
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-05 18:18:42 +0530

    Added React Router and route for home page

--- Expense-Tracker-1/client/src/App.jsx
+++ Expense-Tracker-1/client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<div>
+			<NavBar />
+			<hr />
+			<Routes>
+				<Route path="/">
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</div>
+	);
+};
 
-export default App
+export default App;
