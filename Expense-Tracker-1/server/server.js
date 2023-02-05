8559ddb8d8017f279173a0fab49f051f244e12e7
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-05 18:17:54 +0530

    added cors and express body parser

--- Expense-Tracker-1/server/server.js
+++ Expense-Tracker-1/server/server.js
@@ -1,16 +1,31 @@
 import { config } from "dotenv";
 import express from "express";
 import { connectDB } from "./db/connect.js";
+import cors from "cors";
 
+// configs
 const app = express();
 config(); // loading the .env file variables in the process.env
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
 app.get("/", (req, res) => {
 	res.status(200).json({
 		msg: "Welcome to Expense Tracker API !",
 	});
 });
 
+app.post("/api/transactions", (req, res) => {
+	console.log(req.body);
+	res.status(200).json({
+		msg: "Form Data Recieved...",
+	});
+});
+
+// start
 async function startServer() {
 	try {
 		await connectDB(process.env.MONGO_URI);
