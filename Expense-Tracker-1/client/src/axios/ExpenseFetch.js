31807b649f29c8e36d0455eec4e6de8f44142cb0
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-11 01:22:06 +0530

    added methods for crud for expenseFetch api

--- Expense-Tracker-1/client/src/axios/ExpenseFetch.js
+++ Expense-Tracker-1/client/src/axios/ExpenseFetch.js
@@ -1,5 +1,29 @@
 import axios from "axios";
 
 export const expenseFetch = axios.create({
-	baseURL: "http://localhost:8000/api",
+	baseURL: "http://localhost:8000/api/v1",
 });
+
+export const postTransaction = async (formData) => {
+	return await expenseFetch.post("/transaction", formData, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
+
+export const getAllTransactions = async () => {
+	return await expenseFetch.get("/transactions");
+};
+
+export const getTransaction = async (id) => {
+	return await expenseFetch.get(`/transaction/${id}`);
+};
+
+export const updateTransaction = async (transactionData) => {
+	return await expenseFetch.put(`/transaction/${transactionData.id}`);
+};
+
+export const deleteTransaction = async (id) => {
+	return await expenseFetch.delete(`/transaction/${id}`);
+};
