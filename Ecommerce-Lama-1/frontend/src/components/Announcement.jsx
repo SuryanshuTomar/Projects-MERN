fc2c5c3c0254456126255135301c91f56b6c42c4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-10 18:21:39 +0530

    Announcement Component

---
+++ Ecommerce-Lama-1/frontend/src/components/Announcement.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 30px;
+	background-color: salmon;
+	color: whitesmoke;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-style: 12px;
+	font-weight: bold;
+`;
+
+const Announcement = () => {
+	return <Container>Super deal! Free Shipping on Orders Over $50</Container>;
+};
+export default Announcement;
