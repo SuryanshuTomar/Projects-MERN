8469a92e48419e51c1e1bace91bc5d91d8982dce
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-11 01:22:39 +0530

    Added Transactions component

--- Expense-Tracker-1/client/src/pages/Home.jsx
+++ Expense-Tracker-1/client/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import TransactionForm from "../components/TransactionForm";
+import Transactions from "../components/Transactions";
 
 const Home = () => {
 	return (
 		<div>
+			<Transactions />
 			<TransactionForm />
 		</div>
 	);
