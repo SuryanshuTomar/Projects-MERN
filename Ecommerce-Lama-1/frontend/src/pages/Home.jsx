79af0777ccb11e7f63ac21fd911f71dfe599ed2b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-10 23:53:25 +0530

    added slider and categories

--- Ecommerce-Lama-1/frontend/src/pages/Home.jsx
+++ Ecommerce-Lama-1/frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
 	return (
 		<div>
 			<Announcement />
 			<Navbar />
+			<Slider />
+			<Categories />
 		</div>
 	);
 };
