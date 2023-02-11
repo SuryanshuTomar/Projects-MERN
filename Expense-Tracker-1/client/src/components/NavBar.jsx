1f943bec257f60eed635847c9bf7d3af2953c76c
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-11 16:31:14 +0530

    Navbar Component Style addded

--- Expense-Tracker-1/client/src/components/NavBar.jsx
+++ Expense-Tracker-1/client/src/components/NavBar.jsx
@@ -1,14 +1,36 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.nav`
+	background-color: #e84855;
+	height: 4rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Title = styled.h3`
+	margin-left: 3rem;
+	font-size: 1.5rem;
+`;
+
+const Links = styled.div`
+	width: 20rem;
+	display: flex;
+	justify-content: center;
+	font-size: 1.1rem;
+`;
 
 const NavBar = () => {
 	return (
-		<nav>
-			<h3>Expense Tracker</h3>
-			<NavLink to="/" className="navLink">
-				Home
-			</NavLink>
-         
-		</nav>
+		<Container>
+			<Title>EXPENSE TRACKER</Title>
+			<Links>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/">Categories</NavLink>
+			</Links>
+		</Container>
 	);
 };
+
 export default NavBar;
