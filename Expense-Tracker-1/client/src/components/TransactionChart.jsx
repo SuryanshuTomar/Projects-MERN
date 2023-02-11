db096c3ac6033f813392f380634afe69ae437483
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-11 16:31:27 +0530

    Transaction Chart Component

---
+++ Expense-Tracker-1/client/src/components/TransactionChart.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	flex: 2;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: greenyellow;
+`;
+
+const TransactionChart = () => {
+	return <Container>TransactionChart</Container>;
+};
+export default TransactionChart;
