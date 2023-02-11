b6bb445ace35213aa0bb9dd0e3ebed84255a13a8
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-11 22:29:21 +0530

    router for update and delete transaction

--- Expense-Tracker-1/server/routes/transaction.routes.js
+++ Expense-Tracker-1/server/routes/transaction.routes.js
@@ -3,8 +3,9 @@ import express from "express";
 import {
 	createTransaction,
 	getAllTransactions,
+	deleteTransaction,
+	updateTransaction,
 } from "../controllers/transaction.controller.js";
-
 
 // Create express router
 const router = express.Router();
@@ -12,5 +13,9 @@ const router = express.Router();
 router.post("/transaction", createTransaction);
 
 router.get("/transactions", getAllTransactions);
+
+router.delete("/transaction/:id", deleteTransaction);
+
+router.patch("/transaction/:id", updateTransaction);
 
 export default router;
