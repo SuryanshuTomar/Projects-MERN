541b4b10cb2695fd194d1c438a624c3a2f15b1d6
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-12 16:26:48 +0530

    Products Component

---
+++ Ecommerce-Lama-1/frontend/src/components/Products.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import Product from "./Product";
+import { popularProducts } from "../data/data";
+
+const Container = styled.div`
+	padding: 20px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+`;
+
+const Products = () => {
+	return (
+		<Container>
+			{popularProducts.map((item) => (
+				<Product key={item.id} item={item} />
+			))}
+		</Container>
+	);
+};
+export default Products;
