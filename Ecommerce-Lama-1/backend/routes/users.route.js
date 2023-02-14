b048bd090b46de59c8f19b3c37b5ec082d6b99bf
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-15 00:19:55 +0530

    router file skeleton

--- Ecommerce-Lama-1/backend/routes/users.route.js
+++ Ecommerce-Lama-1/backend/routes/users.route.js
@@ -1,6 +1,11 @@
+// imports
 const express = require("express");
+
+// get the router
 const router = express.Router();
 
+
+// routes
 router.get("/usertest", (req, res) => {
 	res.status(200).json({
 		status: "Success",
