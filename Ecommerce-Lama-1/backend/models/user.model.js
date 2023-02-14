0dfa7355d4fd5da33d0f41ced7cbf946c35b0304
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-15 00:17:26 +0530

    user model

--- Ecommerce-Lama-1/backend/models/user.model.js
+++ Ecommerce-Lama-1/backend/models/user.model.js
@@ -2,8 +2,34 @@
 const mongoose = require("mongoose");
 
 // get Schema
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-   
-})
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			minLength: [3, "Min length for the username is 3"],
+			maxLength: [50, "Max charachter allowed for username is 50"],
+		},
+		email: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			require: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+// create the user Model and export
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
