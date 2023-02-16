1659547f6b2cf374ab52f0358d9e017f9474e57f
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-16 16:39:16 +0530

    added authroize middleware for user and admin

---
+++ Ecommerce-Lama-1/backend/middlewares/verify.middleware.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+	const authHeader = req.headers.token;
+
+	if (!authHeader) return res.status(401).json("Not Authenticated!!");
+
+	const token = authHeader.split(" ")[1];
+	jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+		if (error) return res.status(403).json("Invalid token!!");
+
+		req.user = user;
+		next();
+	});
+};
+
+const verifyTokenAndAuthorize = (req, res, next) => {
+	verifyToken(req, res, () => {
+		if (req.user.id === req.params.id || req.user.isAdmin) {
+			next();
+		} else {
+			return res.status(403).json("Not allowed!!");
+		}
+	});
+};
+
+const verifyTokenAndAuthorizeAdmin = (req, res, next) => {
+	verifyToken(req, res, () => {
+		if (req.user.isAdmin) {
+			next();
+		} else {
+			return res.status(403).json("Not allowed!!");
+		}
+	});
+};
+
+module.exports = {
+	verifyToken,
+	verifyTokenAndAuthorize,
+	verifyTokenAndAuthorizeAdmin,
+};
